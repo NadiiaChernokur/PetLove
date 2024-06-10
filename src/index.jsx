6c6a210b3eb9f3066c050d841ceae213684cade3
NadiiaChernokur <chiernokur5@gmail.com>
2024-06-10 12:59:27 +0200

    88

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
-import { persistor, store } from './redux/store.jsx';
+import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
